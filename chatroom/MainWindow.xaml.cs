3f169ac57d118e6d0dfefa4435727b1e30ca6a63
Pawelelek <pavlo13-13@ukr.net>
2023-03-29 20:28:59 +0300

    Add project files.

---
+++ chatroom/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace chatroom
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IPEndPoint serverEndPoint;
+        UdpClient client = new UdpClient();
+        ObservableCollection<MessageInfo> messages = new ObservableCollection<MessageInfo>();
+        //const string serverAddress = "127.0.0.1";
+        //const short serverPort = 4040;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = messages;
+            string serverAddress = ConfigurationManager.AppSettings["ServerAddress"]!; 
+            short serverPort = short.Parse(ConfigurationManager.AppSettings["ServerPort"]!); 
+            serverEndPoint = new IPEndPoint(IPAddress.Parse(serverAddress), serverPort);
+            //sendbtn.IsEnabled = false;
+            
+        }
+
+        private void SendBtnClick(object sender, RoutedEventArgs e)
+        {
+            SendMessage(msgTextBox.Text);
+        }
+
+        private void JoinBtnClick(object sender, RoutedEventArgs e)
+        {
+            //if (!sendbtn.IsEnabled)
+            //{
+            //    sendbtn.IsEnabled = true;
+            //}
+            SendMessage("$<join>");
+            Listen();
+        }
+        private async void SendMessage(string message)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            await client.SendAsync(data, data.Length, serverEndPoint);
+        }
+        private async void Listen()
+        {
+          while (true)
+          {
+              var res = await client.ReceiveAsync();
+              string message = Encoding.Unicode.GetString(res.Buffer);
+              messages.Add(new MessageInfo(message));
+          }
+           
+        }
+        private void LeaveBtnClick(object sender, RoutedEventArgs e)
+        {
+            //if (sendbtn.IsEnabled)
+            //{
+            //    sendbtn.IsEnabled = false;
+            //}
+            //if (leavebtn.IsEnabled)
+            //{
+            //    leavebtn.IsEnabled = false;
+            //}
+            //SendMessage("$<leave>");
+        }
+    }
+    class MessageInfo
+    {
+        public string Message { get; set; }
+        public DateTime Time { get; set; }
+        public MessageInfo(string msg)
+        {
+            Message = msg;
+            Time = DateTime.Now;
+        }
+        public override string ToString()
+        {
+            return $"{Message}, {Time.ToShortDateString()}";
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace server
+{
+    class ChatServer
+    {
+        const short port = 4040;
+        const string JOIN_CMD = "$<join>";
+        UdpClient server = new UdpClient(port);
+        HashSet<IPEndPoint> members = new HashSet<IPEndPoint>();
+        IPEndPoint clientEndPoint = null;
+        private void AddMember(IPEndPoint member)
+        {
+            members.Add(member);
+            Console.WriteLine("Member was added!!!");
+        }
+        private void SendToAll(byte[] data)
+        {
+            foreach (IPEndPoint member in members)
+            {
+                server.SendAsync(data, data.Length, member);
+            }
+
+
+
+        }
+        public void Start()
+        {
+            while (true)
+            {
+                byte[] data = server.Receive(ref clientEndPoint);
+                string message = Encoding.Unicode.GetString(data);
+                Console.WriteLine($" {message} at {DateTime.Now.ToShortTimeString()}" +
+                $" from {clientEndPoint}");
+                if (JOIN_CMD == message)
+                {
+                    AddMember(clientEndPoint);
+                }
+                else
+                {
+                    SendToAll(data);
+                }
+
+
+
+            }
+        }
+
+
+
+    }
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            ChatServer server = new ChatServer();
+            server.Start();
+           
+
+                //switch (message)
+                //{
+                //    case JOIN_CMD:
+                        
+                //        break;
+                //    default:
+                //        break;
+                //}
+            
+        }
+    }
+}
